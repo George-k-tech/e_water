a14df3b12b8b8b8b5f61693f740ddd0c451df88b
George-k-tech <kariukigeorge511@gmail.com>
2023-04-21 22:01:27 +0300

    new new_item

--- lib/screens/grocery.dart
+++ lib/screens/grocery.dart
@@ -1,14 +1,29 @@
+import 'package:e_water/Widget/new_item.dart';
 import 'package:e_water/data/dummy_data.dart';
 import 'package:flutter/material.dart';
 
-class GroceryScreen extends StatelessWidget {
+class GroceryScreen extends StatefulWidget {
   const GroceryScreen({super.key});
+
+  @override
+  State<GroceryScreen> createState() => _GroceryScreenState();
+}
+
+class _GroceryScreenState extends State<GroceryScreen> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your grocery'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
